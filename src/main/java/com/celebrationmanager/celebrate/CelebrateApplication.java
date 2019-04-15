9ca7ead4837e9b4f69e399c6b84a757ee2d31de5
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-04-15 17:28:48 -0400

    created shell project

---
+++ src/main/java/com/celebrationmanager/celebrate/CelebrateApplication.java
@@ -0,0 +1,13 @@
+package com.celebrationmanager.celebrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelebrateApplication.class, args);
+    }
+
+}
